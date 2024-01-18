3b2f0924628f3bfd36aacdb2492f85e0724e54d8
Gamesiliketoplay <Jamespaouellet@gmail.com>
2024-01-18 08:58:33 -0500

    Searcher working

--- src/main/java/project/StudentManager.java
+++ src/main/java/project/StudentManager.java
@@ -1,57 +1,19 @@
 package project;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StudentManager {
-    public ArrayList<Classes> GetCourses() {
-        String classesFile = "src/main/java/project/Classes.json";
-        Gson gson = new Gson();
+    private int x;
 
-        try (FileReader reader = new FileReader(classesFile)) {
-            Type listType = new TypeToken<ArrayList<Classes>>() {
-            }.getType();
+    public Student studentSearch(ArrayList<Student> students, String searchLastName) {
 
-            return gson.fromJson(reader, listType);
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (Student student : students) {
+            if (student.getlastName().equals(searchLastName)) {
+                return student;
+            }
+            x++;
         }
-        return null;
-    }
-
-    public ArrayList<Enrolments> getGrades() {
-        String enrolmentsFile = "src/main/java/project/Enrolments.json";
-        Gson gson = new Gson();
-
-        try (FileReader reader = new FileReader(enrolmentsFile)) {
-            Type listType = new TypeToken<ArrayList<Enrolments>>() {
-            }.getType();
-            return gson.fromJson(reader, listType);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return new ArrayList<>(); // Return an empty list in case of an error
-    }
-
-    public ArrayList<Student> GetStundentInfo() {
-        String studentFile = "src/main/java/project/Student.json";
-        Gson gson = new Gson();
-
-        try (FileReader reader = new FileReader(studentFile)) {
-            Type listType = new TypeToken<ArrayList<Student>>() {
-            }.getType();
-
-            return gson.fromJson(reader, listType);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         return null;
     }
 }
